b38f0602e55b8dbca57028470012114eeb0ff18e
David Santos <david@dsantosdev.com>
2015-07-02 17:41:41 -0300

    test: rename 'cpf_cnpj_test' => 'cpf_cnpj_helpers_test'
    Since the current tests test functions that identify and format
    CPFs/CNPJs, rename the file to reflect that and rename the test class
    accordingly.

--- test/cpf_cnpj_helpers_test.rb
+++ test/cpf_cnpj_helpers_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class CpfCnpjTest
+class CpfCnpjHelpersTest < CpfCnpjTest
 	class VersionTest < CpfCnpjTest
 		def test_that_it_has_a_version_number
 			refute_nil ::CpfCnpj::VERSION
