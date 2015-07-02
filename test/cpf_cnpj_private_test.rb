bd4163dc50d105313f18a48fc47df76141a93340
David Santos <david@dsantosdev.com>
2015-07-02 20:32:16 -0300

    test private: mod11_check_digit: don't consider a string with non-numeric characters as valid

--- test/cpf_cnpj_private_test.rb
+++ test/cpf_cnpj_private_test.rb
@@ -12,6 +12,17 @@ class CpfCnpjPrivateTest < CpfCnpjTest
 		def test_returns_false_when_incorrect
 			refute(CpfCnpj._mod11_check_digit(INVALID_CPF[0..-2], MAX_MULTI_CPF))
 		end
+
+		# If the string is not a number, String#to_i returns zero. So, if the digits
+		# are converted using #to_i, the check succeeds if the string has a non-numeric character
+		# in a place where a zero would make the check succeed.
+		def test_returns_false_when_non_digits
+			# if any character is zero
+			refute(CpfCnpj._mod11_check_digit(VALID_CPF_WITH_ZERO.sub('0', 'a'), MAX_MULTI_CPF))
+			# the last character is not used to calculate, just to verify, so it could be vulnerable
+			# even if the others are checked for range
+			refute(CpfCnpj._mod11_check_digit(VALID_CPF_END_ZERO.sub(/0\z/, 'a'), MAX_MULTI_CPF))
+		end
 	end
 
 	class Mod11CheckTest < CpfCnpjTest
