04414abe83e302beaffc9956706c0d7d7fbdd225
David Santos <david@dsantosdev.com>
2015-07-02 17:07:33 -0300

    implement type_of

--- lib/cpf_cnpj.rb
+++ lib/cpf_cnpj.rb
@@ -1,5 +1,15 @@
 require "cpf_cnpj/version"
 
 module CpfCnpj
-	# Your code goes here...
+	def self.type_of(cpf_cnpj)
+		return nil if cpf_cnpj.nil?
+		unless cpf_cnpj.is_a?(String)
+			raise(ArgumentError, "argument must be a string or nil")
+		end
+		case cpf_cnpj.length
+		when 11 ; :cpf
+		when 14 ; :cnpj
+		else ; nil
+		end
+	end
 end
