eac52e6dd79574d244f707e83be1904866bd4824
David Santos <david@dsantosdev.com>
2015-07-02 21:42:44 -0300

    test validators: test valid_cnpj?

--- test/cpf_cnpj_validators_test.rb
+++ test/cpf_cnpj_validators_test.rb
@@ -15,4 +15,19 @@ class CpfCnpjValidatorsTest < CpfCnpjTest
 			refute(CpfCnpj.valid_cpf?(VALID_MOD11_CPF_TOO_LONG))
 		end
 	end
+
+	class ValidCnpjTest < CpfCnpjTest
+		def test_returns_true_when_valid
+			assert(CpfCnpj.valid_cnpj?(VALID_CNPJ))
+		end
+
+		def test_returns_false_when_invalid
+			refute(CpfCnpj.valid_cnpj?(INVALID_CNPJ))
+		end
+
+		def test_returns_false_when_not_cnpj
+			refute(CpfCnpj.valid_cnpj?(VALID_MOD11_CNPJ_TOO_SHORT))
+			refute(CpfCnpj.valid_cnpj?(VALID_MOD11_CNPJ_TOO_LONG))
+		end
+	end
 end
